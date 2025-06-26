fba2703fc66de2511a0fa4f4e0d2735449823211
Glados <muojeso90@gmail.com>
2025-06-26 20:06:46 +0900

    [ADD] 계산기 프로젝트 , jsp 학습, 작동구조 ,장점과 한계

--- Servlet&JSP/jsp/workspace/JSPPrj/src/main/java/com/newlecture/web/Calc3.java
+++ Servlet&JSP/jsp/workspace/JSPPrj/src/main/java/com/newlecture/web/Calc3.java
@@ -3,6 +3,9 @@ package com.newlecture.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -23,78 +26,55 @@ public class Calc3 extends HttpServlet{
 	@Override
 	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		ServletContext application = request.getServletContext();
-		HttpSession session = request.getSession();
 		Cookie[] cookies = request.getCookies();
 		
 		
-		
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter writer = response.getWriter();
 
-		
-		
-		String v_ = request.getParameter("v");
-		String op = request.getParameter("operator");
-		
-		
-		
-		int v = 0;
-		if(!v_.equals("")) v = Integer.parseInt(v_);
-		
-		if(op.equals("=")) {
 			
-//			int x = (Integer) application.getAttribute("value");
-//			int x = (Integer) session.getAttribute("value");
-			int x =0;
-			
+		
+		String value = request.getParameter("value");
+		String operator = request.getParameter("operator");
+		String dot = request.getParameter("dot");
+		
+		String exp = "";
+		if(cookies != null) {
 			for(Cookie c : cookies) {
-				if(c.getName().equals("value")) {
-					x = Integer.parseInt(c.getValue());
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
 					break;
 				}
 			}
 			
-			int y = v;
-//			String operator = (String)application.getAttribute("op");
-//			String operator = (String)session.getAttribute("op");
-			
-			String operator = "";
-			for(Cookie c : cookies) {
-				if(c.getName().equals("op")) {
-					operator = c.getValue();
-					break;
-				}
-			}
-			
-			int result = 0;
-			
-			if(operator.equals("+"))
-				result = x + y;
-			else
-				result = x - y;
-			
-			writer.println("답은 " + result + " 입니다.");
-		} else {
-//			application.setAttribute("value", v);
-//			application.setAttribute("op", op);
-			
-//			session.setAttribute("value", v);
-//			session.setAttribute("op", op);
-			
-			Cookie valueCookie = new Cookie("value", String.valueOf(v));
-			Cookie opCookie = new Cookie("op", op);
-			valueCookie.setPath("/calc2");
-			valueCookie.setMaxAge(24*60*60);
-			
-			opCookie.setPath("/calc2");
-			
-			response.addCookie(valueCookie);
-			response.addCookie(opCookie);
-			
-			response.sendRedirect("calc2.html");
 		}
+		
+		if(operator != null && operator.equals("=")) {
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("graal.js");
+			try {
+				exp = String.valueOf(engine.eval(exp));
+			} catch (ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if(operator != null && operator.equals("C")) { 
+			exp = "";
+		}
+		else {
+			exp += (value == null)?"":value;
+			exp += (operator == null)?"":operator;
+			exp += (dot== null)?"":dot;
+		}
+		
+		
+		Cookie expCookie = new Cookie("exp", exp);
+		if(operator != null && operator.equals("C"))
+			expCookie.setMaxAge(0);
+		
+		expCookie.setPath("/calc3");
+		response.addCookie(expCookie);
+		response.sendRedirect("calcpage");
+		
+		
 		
 	}
 
--- Servlet&JSP/jsp/workspace/JSPPrj/src/main/webapp/calculator.jsp
+++ Servlet&JSP/jsp/workspace/JSPPrj/src/main/webapp/calculator.jsp
@@ -1,3 +1,10 @@
+<%
+	int x = 3;
+	int y = 4;
+%>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -23,7 +30,7 @@ input{
 	<form action="calc3" method="post">
 		<table>
 			<tr>
-				<td class="output" colspan="4">0</td>				
+				<td class="output" colspan="4">${3+4 }</td>				
 			</tr>
 			<tr>
 				<td><input type="submit" name="operator" value="CE" /></td>
